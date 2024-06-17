88c6f31e1f2241fc2cbb5e68b58cb55874e6937c
Just Earth - 14 <kamauwaweru333@gmail.com>
2024-06-18 00:30:12 +0300

    Initial commit

--- main.go
+++ main.go
@@ -1,50 +1,16 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"log"
-	"net/http"
-
-	"github.com/gorilla/mux"
 )
 
-type Article struct {
-	Title   string `json:"Title"`
-	Desc    string `json:"desc"`
-	Content string `json:"content"`
-}
-
-type Articles []Article
-
-func allArticles(w http.ResponseWriter, r *http.Request) {
-	articles := Articles{
-		Article{Title: "Test Title", Desc: "Test Description", Content: "Hello World"},
-	}
-
-	fmt.Println("Endpoint Hit: All Article Endpoint")
-	json.NewEncoder(w).Encode(articles)
-}
-
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Homepage Endpoint Hit")
-
-}
-
-func testPostArticles(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Test POST endpoint worked")
-
-}
-
-func handleRequests() {
-
-	myRouter := mux.NewRouter().StrictSlash(true)
-	myRouter.HandleFunc("/", homePage)
-	myRouter.HandleFunc("/articles", allArticles).Methods("GET")
-	myRouter.HandleFunc("/articles", testPostArticles).Methods("POST")
-	log.Fatal(http.ListenAndServe(":8081", myRouter))
+func Calculate(x int) (result int) {
+	result = x + 2
+	return result
 }
 
 func main() {
-	handleRequests()
+	fmt.Println("Go testing tutorial")
+	result := Calculate(2)
+	fmt.Println(result)
 }
